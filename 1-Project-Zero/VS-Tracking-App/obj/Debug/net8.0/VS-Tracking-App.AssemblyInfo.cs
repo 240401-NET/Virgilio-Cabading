8e5f050c6238b2e5c5b6aa8db4f9242f8c68da13
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2024-04-10 08:36:29 -0500

    project zero

--- 1-Project-Zero/VS-Tracking-App/obj/Debug/net8.0/VS-Tracking-App.AssemblyInfo.cs
+++ 1-Project-Zero/VS-Tracking-App/obj/Debug/net8.0/VS-Tracking-App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VS-Tracking-App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d322b469b6148beb4342f0b7b25e10c75b7f4d23")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebcff9ee24b0a16154f2c4070e29ad973609e2fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("VS-Tracking-App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VS-Tracking-App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
